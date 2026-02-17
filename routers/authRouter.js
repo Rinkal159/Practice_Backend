7d825b8da1b8ae08157b917ef7d37e68673e2243
rinkal159 <rinkalsingapuri@gmail.com>
2026-02-17 21:28:42 +0530

    Groups auth routes.

---
+++ routers/authRouter.js
@@ -0,0 +1,14 @@
+import express from "express";
+import errorHandler from "../middleware/errorHandler.js";
+import authentication from "../middleware/authentication.js";
+import signup from "../controllers/signup.js";
+import login from "../controllers/login.js";
+import logout from "../controllers/logout.js";
+
+const authRouter = express.Router();
+
+authRouter.post("/signup", errorHandler(signup));
+authRouter.post("/login", errorHandler(login));
+authRouter.get("/logout", errorHandler(authentication), errorHandler(logout));
+
+export default authRouter;
