4919048b99c719e2ad5dede85cbe200ab2688022
rinkal159 <rinkalsingapuri@gmail.com>
2026-02-17 21:27:48 +0530

    /get : get all notes.

--- controllers/getNotes.js
+++ controllers/getNotes.js
@@ -1,9 +1,14 @@
-import {notes} from "../model/Notes.js";
+import Notes from "../model/Notes.js";
 
 const getNotes = async (req, res) => {
+    const id = req.id;
+
+    const notes = await Notes.find({ userId: id }).select("title description");
+
     if (notes.length == 0) {
-        throw new Error("Nothing to fetch. Please add notes.")
+        throw new Error('Please add notes to fetch them.');
     }
+
     res.status(200).send(notes);
 }
 
