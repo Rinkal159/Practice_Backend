e39a57c17e32e519eaf82b3b90584f317ef0063c
rinkal159 <rinkalsingapuri@gmail.com>
2026-02-17 21:27:41 +0530

    /post : add a note.

---
+++ controllers/addNote.js
@@ -0,0 +1,22 @@
+import Notes from "../model/Notes.js";
+
+const addNote = async (req, res) => {
+    const userId = req.id;
+    const { title, description } = req.body;
+
+    if (!title || !description) {
+        throw new Error("Title and Description are required.");
+    }
+
+    const newNote = new Notes({
+        userId, title, description
+    });
+
+    newNote.save();
+
+    res.status(201).send(`Note named "${title}" is successfully added.`);
+
+
+}
+
+export default addNote;
