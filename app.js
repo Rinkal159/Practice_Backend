28e2286e6d1d20c0cda7b3769953d96a96380fa5
rinkal159 <rinkalsingapuri@gmail.com>
2026-02-17 21:24:11 +0530

    Connects to database, and runs two routers.

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
-import generalRouter from "./routers/general.js";
+import connect from "./config/database.js";
+import cookieParser from "cookie-parser";
+
+import authRouter from "./routers/authRouter.js";
+import coreRouter from "./routers/coreRouter.js";
 
 const app = express();
 dotenv.config();
 app.use(express.json());
+app.use(cookieParser());
 
-app.get(/^\/.$/, (req, res) => {
-    res.send("My response");
-});
-
-app.use("/", generalRouter);
+app.use("/", authRouter);
+app.use("/", coreRouter);
 
 const port = process.env.PORT;
-app.listen(port, () => {
-    console.log("server success");
 
-})
+connect()
+    .then(() => {
+        console.log("database success");
+        app.listen(port, () => {
+            console.log("server success");
+
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    })
