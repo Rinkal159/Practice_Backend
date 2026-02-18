735c26c534bf63eb8e5b5291c09f0a5f97b92acf
rinkal159 <rinkalsingapuri@gmail.com>
2026-02-18 18:05:24 +0530

    Corrects imports.

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,9 +1,9 @@
 import express from "express";
 import errorHandler from "../middleware/errorHandler.js";
 import authentication from "../middleware/authentication.js";
-import signup from "../controllers/signup.js";
-import login from "../controllers/login.js";
-import logout from "../controllers/logout.js";
+import signup from "../controllers/auth/signup.js";
+import login from "../controllers/auth/login.js";
+import logout from "../controllers/auth/logout.js";
 
 const authRouter = express.Router();
 
